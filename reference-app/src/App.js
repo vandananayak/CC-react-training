9034ed58649f00917490d63e6838d25669f6065c
Vandana Nayak <nvandana1@gmail.com>
2021-09-29 17:39:34 +0530

    ref:Newcomponent created

--- reference-app/src/App.js
+++ reference-app/src/App.js
@@ -1,6 +1,18 @@
+import Search from "./Search";
+
+//this is component
+const title="this is title"
 function App() {
   return (
-    <div>Hello world</div>
+    <>
+    
+    
+    <div>
+      <h1>Hello {title}</h1>
+    </div>
+    <Search></Search>
+    <button>Click</button>
+    </>
   );
 }
 
---
+++ reference-app/src/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Search = (props) => (
+            <>
+            <label htmlFor="search">Search</label>
+            <input type="text" id="search"></input>
+            </>
+    );
+
+export default Search;
